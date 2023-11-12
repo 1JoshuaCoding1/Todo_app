9a78db8db7180c2ecba12c2b2c876a33d0033eab
aldione <aldionejoshua.pancho.21@usjr.edu.ph>
2023-11-12 10:52:29 +0800

    todoooo app

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'todo_item.dart';
+import 'todo_service.dart';
+
+void main() async {
+
+  await Hive.initFlutter(); // Initialize Hive data base
+  Hive.registerAdapter(TodoItemAdapter());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final TodoService _todoService = TodoService();
+
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hive TODO List',
+      debugShowCheckedModeBanner: false,
+      home: FutureBuilder(
+        future: _todoService.getAllTodos(),
+        builder: (context, AsyncSnapshot<List<TodoItem>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return TodoListPage(snapshot.data ?? []);
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+}
+
+class TodoListPage extends StatefulWidget {
+  final List<TodoItem> todos;
+
+  const TodoListPage(this.todos, {Key? key}) : super(key: key);
+
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final TodoService _todoService = TodoService();
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hive TODO List"),
+        backgroundColor: Colors.black,
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box<TodoItem>('todoBox').listenable(),
+        builder: (context, Box<TodoItem> box, _) {
+          return ListView.builder(
+            itemCount: box.length,
+            itemBuilder: (context, index) {
+              var todo = box.getAt(index);
+              return ListTile(
+                title: Text(todo!.title),
+                leading: Checkbox(
+                  value: todo.isCompleted,
+                  onChanged: (val) {
+                    _todoService.toggleCompleted(index, todo);
+                  },
+                ),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    _todoService.deleteTodo(index);
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: () async {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Add Todo'),
+                content: TextField(
+                  controller: _controller,
+                ),
+                actions: [
+                  ElevatedButton(
+                    child: const Text('Add'),
+                    onPressed: () {
+                      if (_controller.text.isNotEmpty) {
+                        var todo = TodoItem(_controller.text);
+                        _todoService.addTodo(todo);
+                        _controller.clear();
+                        Navigator.pop(context);
+                      }
+                    },
+                  )
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
